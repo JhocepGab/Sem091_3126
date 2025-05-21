d8743039003558a2131fe4e3dbf3db16616c4629
Jhocep Verastegui <snkje123@gmail.com>
2025-05-21 16:26:02 -0500

    Agregar archivos de proyecto.

---
+++ MyejemplClass09/Areas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyejemplClass09
+{
+    public class Areas
+    {
+        //muchos metotodos
+        public double AreaCuadrado(double l)
+        {
+            double a;
+            a = l * l;
+            return a;
+        }
+        public double AreaTriangulo(double b, double h)
+        {
+            return (b * h) / 2;
+        }
+        public double AreaRectangulo(double l, double a)
+        {
+            double r;
+            r = l * a;
+            return r;
+        }
+        public double AreaCirculo(double r)
+        {
+            double a = Math.PI * (r * r);
+            return a;
+        }
+    }
+}
---
+++ MyejemplClass09/Perimetros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyejemplClass09
+{
+    public class Perimetros
+    {
+        //metodo para rectanguloo
+        //para el circulo 
+        public double PerimetroRectangulo(double l, double a)
+        {
+            double r = (l * 2) + (a * 2);
+            return r;
+        }
+        public double PerimetroDelCircule(double r)
+        {
+            double a = 2 * Math.PI * r;
+            return a;
+        }
+    }
+}
